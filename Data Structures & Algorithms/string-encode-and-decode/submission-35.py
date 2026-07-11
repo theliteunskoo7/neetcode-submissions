d02d8634d72438bff8147d87d0ba054771eb7b3e
theliteunskoo7 <ai4vijay@gmail.com>
2026-07-11 19:55:07 +0530

    Add: string-encode-and-decode - submission-35

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-35.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        s=""
+        for a in strs:
+            n = str(len(a))
+            if len(n) ==1:
+                n = "00"+n
+            elif len(n) == 2:
+                n ="0" + n
+            s+=n + a
+        return s 
+
+    def decode(self, s: str) -> List[str]:
+        op=[]
+        i=0
+        while  i < len(s):
+            l = int(s[i:i+3])
+            stri = s[(i+3):(i+3+l)]
+            op.append(stri)
+            i = i + l + 3
+        return op
+        
